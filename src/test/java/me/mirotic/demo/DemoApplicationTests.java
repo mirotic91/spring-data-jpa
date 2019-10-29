2674cc3c43fdc9731200b59b2e5d35f3bbe9fd30
jupark <jupark@rsupport.com>
2019-10-29 17:33:40 +0900

    initial commit

---
+++ src/test/java/me/mirotic/demo/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package me.mirotic.demo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Description;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @DisplayName("컨텍스트 로드")
+    @Test
+    void contextLoads() {
+    }
+
+}
